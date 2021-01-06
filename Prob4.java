d265a37c05685a8e9a06b880122c46b11600a017
Aleksandra <ab0200@students.necsd.net>
2021-01-06 05:10:44 +0000

    prob 4

--- Prob4.java
+++ Prob4.java
@@ -2,6 +2,39 @@ import java.util.Scanner;
 
 public class Prob4 {
   public static void main(String[] args) {
+    Scanner scan = new Scanner(System.in);
+    System.out.println("Enter two characters:");
+
+    String s = scan.nextLine();
+    if (s.charAt(0) == 'M'){
+      System.out.println("Mathmatics");
+
+    else if (s.charAt(0) == 'C');
+     System.out.println("Computer Science");
+
+    else if (s.charAt(0) == 'I');
+     System.out.println("Information technology");
+
+    }
+
+    String s = scan.nextLine();
+    if (s.charAt(0) == '1'){
+      System.out.println("freshmen");
+
+    else if (s.charAt(0) == '2');
+     System.out.println("sophmore");
+
+    else if (s.charAt(0) == '3');
+     System.out.println("Junior");
+
+    else if (s.charAt(0) == '4');
+     System.out.println("Senior");
+
+    else System.out.println ("Thats incorrect");
+
+
+
+    
 
 
   }
